ae6e9a2a36e354d3184dd28b622213bff7406f5e
WMF00 <wfriedrich593@g.rwu.edu>
2021-10-31 16:05:50 -0400

    Input 1 and 2

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-def piglatin():
-    words = str(input("Input Sentence:")).split()
-    for word in words:
-        print(word[1:] + word[0] + "ay", end = " ")
-    print ()
+
+def input1():
+    sentence = input ("input sentence: ")
+    for words in sentence.split():
+        print(words)
+
+def input2():
+    sentence = input("input sentence: ")
+    words = sentence.replace(" ", "\n")
+    print(words)
+
+
 
 
 if __name__ == '__main__':
-    piglatin()
+    input1()
+    input2()
